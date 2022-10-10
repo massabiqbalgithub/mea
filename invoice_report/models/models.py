a20af0b6568cf5550c5df0a5fec787bdfe0d4b93
massabiqbalgithub <115137701+massabiqbalgithub@users.noreply.github.com>
2022-10-10 18:58:48 +0500

    Add files via upload

--- invoice_report/models/models.py
+++ invoice_report/models/models.py
@@ -23,33 +23,41 @@ class AccountMoveInh(models.Model):
     def get_vat(self):
         vat_total = 0
         for line in self.invoice_line_ids:
-            product = self.env['product.pricelist.item'].search(
-                [('product_id', '=', line.product_id.id), ('pricelist_id', '=', 1)], limit=1)
-            vat_total = vat_total + (((product.fixed_price * line.quantity) - ((product.fixed_price* line.quantity)*(line.discount/100))) * (line.product_id.taxes_id[0].amount/100))
+            if line.display_type != 'line_note' and line.display_type != 'line_section':
+                product = self.env['product.pricelist.item'].search(
+                    [('product_id', '=', line.product_id.id), ('pricelist_id', '=', 1)], limit=1)
+                vat_total = vat_total + (((line.price_unit * line.quantity) - ((line.price_unit* line.quantity)*(line.discount/100))) * (line.product_id.taxes_id[0].amount/100))
         return vat_total
 
     def get_discount_amount(self):
         discount_total = 0
         for line in self.invoice_line_ids:
-            discount_total = discount_total + (line.get_line_discount() * line.quantity)
+            if line.display_type != 'line_note' and line.display_type != 'line_section':
+                product = self.env['product.pricelist.item'].search(
+                    [('product_id', '=', line.product_id.id), ('pricelist_id', '=', 1)], limit=1)
+                tax = ((line.product_id.taxes_id[0].amount/100) * (line.price_unit * line.quantity)) +(line.price_unit* line.quantity)
+                discount_total = discount_total + ((tax*line.quantity) * (line.discount / 100))
+        print(discount_total)
         return "{:.2f}".format(discount_total)
 
     def get_taxable_amount(self):
         discount_total = 0
         for line in self.invoice_line_ids:
-            product = self.env['product.pricelist.item'].search(
-                [('product_id', '=', line.product_id.id), ('pricelist_id', '=', 1)], limit=1)
-            discount_total = discount_total + ((product.fixed_price* line.quantity) - (product.fixed_price* line.quantity)*(line.discount/100))
+            if line.display_type != 'line_note' and line.display_type != 'line_section':
+                product = self.env['product.pricelist.item'].search(
+                    [('product_id', '=', line.product_id.id), ('pricelist_id', '=', 1)], limit=1)
+                discount_total = discount_total + ((line.price_unit* line.quantity) - (line.price_unit* line.quantity)*(line.discount/100))
         return discount_total
 
     def get_vat_amount(self):
         vat_total = 0
         for line in self.invoice_line_ids:
-            product = self.env['product.pricelist.item'].search(
-                [('product_id', '=', line.product_id.id), ('pricelist_id', '=', 1)], limit=1)
-            vat_total = vat_total + (((product.fixed_price * line.quantity) - (
-                        (product.fixed_price * line.quantity) * (line.discount / 100))) * (
-                                                 line.product_id.taxes_id[0].amount / 100))
+            if line.display_type != 'line_note' and line.display_type != 'line_section':
+                product = self.env['product.pricelist.item'].search(
+                    [('product_id', '=', line.product_id.id), ('pricelist_id', '=', 1)], limit=1)
+                vat_total = vat_total + (((line.price_unit * line.quantity) - (
+                            (line.price_unit * line.quantity) * (line.discount / 100))) * (
+                                                     line.product_id.taxes_id[0].amount / 100))
         return vat_total
 
 
@@ -66,13 +74,17 @@ class AccountMoveLine(models.Model):
         return product.fixed_price
 
     def get_unit_price_customer_tax(self):
-        product = self.env['product.pricelist.item'].search([('product_id', '=', self.product_id.id), ('pricelist_id', '=', 1)], limit=1)
-        return (product.fixed_price*self.quantity +((product.fixed_price*self.quantity)*(self.product_id.taxes_id[0].amount/100)))
+        # print(self.display_type == '')
+        if self.display_type != 'line_note' and self.display_type != 'line_section':
+            product = self.env['product.pricelist.item'].search([('product_id', '=', self.product_id.id), ('pricelist_id', '=', 1)], limit=1)
+            return (self.price_unit*self.quantity +((self.price_unit*self.quantity)*(self.product_id.taxes_id[0].amount/100)))
+        else:
+            return 0
 
     def get_line_discount(self):
         product = self.env['product.pricelist.item'].search(
             [('product_id', '=', self.product_id.id), ('pricelist_id', '=', 1)], limit=1)
-        return (product.fixed_price * self.quantity) * (self.discount / 100)
+        return ((self.price_unit * self.quantity)*self.quantity) * (self.discount / 100)
 
 
 
